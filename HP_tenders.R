ea890ba17905a2e979884b59c669d7ee22ee344a
preethical <preethi@deobald.ca>
2021-03-30 20:25:08 -0600

    just read the hp tenders files

---
+++ HP_tenders.R
@@ -0,0 +1,12 @@
+##HP geospatial stuff
+
+HPtenders <- read.csv("combined_tenders_results_with_repeats.csv",row.names = NULL,
+                      stringsAsFactors = FALSE)
+
+HPawards <- read.csv("health_award.csv",row.names = NULL,
+                     stringsAsFactors = FALSE)
+
+HPtend <- read.csv("final_merged.csv",row.names = NULL,
+                   stringsAsFactors = FALSE)
+
+
